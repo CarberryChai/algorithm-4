e4b971d10f3b514de0d563981caba0264544ff09
CarberryChai <CarberryChai@gmail.com>
2020-04-22 20:13:07 +0800

    update

--- chapter_1/exercise/Task_1_3.java
+++ chapter_1/exercise/Task_1_3.java
@@ -7,6 +7,23 @@ public class Task_1_3 {
         int z = parse(args[2]);
         if (x == y && y == z) System.out.println("equal!");
         else System.out.println("not equal!");
+        // 1.7
+        int sum1 = 0;
+        for (int i = 1; i < 1000; i++)
+            for (int j = 0; j < i; j++)
+                sum1++;
+        System.out.println(sum1);
+
+        int sum2 = 0;
+        for (int i = 1; i < 1000; i *= 2)
+            for (int j = 0; j < i; j++)
+                sum2++;
+        System.out.println(sum2);
+
+        // 1.8
+        System.out.println('b');
+        System.out.println('b' + 'c');
+        System.out.println((char)('a' + 4));
     }
 
     public static int parse(String x) {
