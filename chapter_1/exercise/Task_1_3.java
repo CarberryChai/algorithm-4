5bd2579e2f3d38d7d82b10b2668f32f6203b8593
CarberryChai <CarberryChai@gmail.com>
2020-04-22 19:35:11 +0800

    task 1.1.3

---
+++ chapter_1/exercise/Task_1_3.java
@@ -0,0 +1,18 @@
+package chapter_1.exercise;
+
+public class Task_1_3 {
+    public static void main(String[] args) {
+        int x = parse(args[0]);
+        int y = parse(args[1]);
+        int z = parse(args[2]);
+        if (x == y && y == z) {
+            System.out.println("equal!");
+        } else {
+            System.out.println("not equal!");
+        }
+    }
+
+    public static int parse(String x) {
+        return Integer.parseInt(x);
+    }
+}
