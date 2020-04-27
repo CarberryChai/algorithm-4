c5632f34ee41c050f7c8add44f81d304ccfeb707
CarberryChai <CarberryChai@gmail.com>
2020-04-27 12:38:30 +0800

    update

--- chapter_1/exercise/Stack.java
+++ chapter_1/exercise/Stack.java
@@ -7,35 +7,35 @@ public class Stack<T> implements Iterable<T> {
     private T[] a;
     private int N;
 
-    public Stack(int cap) {
-        this.a = (T[]) new Object[cap];
+    public Stack() {
+        this.a = (T[]) new Object[10];
     }
 
     public static void main(String[] args) {
-        Stack<String> fixedCap = new Stack(10);
-        fixedCap.push("to");
-        fixedCap.push("be");
-        fixedCap.push("or");
-        fixedCap.push("not");
-        for (String item : fixedCap){
-            System.out.println(item);
-        }
+       String s = "()";
+        System.out.println(s.charAt(0) == s.charAt(1));
     }
 
     public boolean isEmpty() {
         return N == 0;
     }
 
+    public boolean isFull() {
+        return N == a.length;
+    }
+
     public int size() {
         return N;
     }
-    private void resize(int max){
+
+    private void resize(int max) {
         T[] temp = (T[]) new Object[max];
-        for (int i = 0; i < a.length; i++) {
+        for (int i = 0; i < temp.length; i++) {
             temp[i] = a[i];
         }
         this.a = temp;
     }
+
     public void push(T s) {
         if (N == a.length) this.resize(a.length * 2);
         this.a[N++] = s;
@@ -44,7 +44,7 @@ public class Stack<T> implements Iterable<T> {
     public T pop() {
         T item = this.a[--N];
         a[N] = null;
-        if (N > 0 && N == a.length / 4) resize(a.length/2);
+        if (N > 0 && N == a.length / 4) resize(a.length / 2);
         return item;
     }
 
@@ -63,6 +63,7 @@ public class Stack<T> implements Iterable<T> {
 
     private class ReverseArrayIterator implements Iterator<T> {
         private int i = N;
+
         @Override
         public boolean hasNext() {
             return i > 0;
